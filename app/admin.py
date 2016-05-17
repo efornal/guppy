f05f9f95fee85b949da29023dc59e2960c34e3f7
esteban <efornal@gmail.com>
2016-05-17 12:21:53 -0300

    sending mails to project managers

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 from django.contrib import admin
 from app.models import Project
 from app.models import Integration
@@ -5,8 +6,12 @@ from app.models import Change
 from app.models import Responsable
 from app.models import Generated
 from app.models import Integrate
+from django.contrib.auth.models import User
 from django.conf.locale.es import formats as es_formats
+from django.core.mail import send_mass_mail
 import logging
+from django.conf import settings
+from django.contrib import messages
 
 es_formats.DATETIME_FORMAT = "d-m-Y H:i"
 
@@ -52,12 +57,45 @@ class ChangeAdmin(admin.ModelAdmin):
     ordering = ('project',)
 
     def save_model(self, request, obj, form, change):
-        project = Project.objects.get(pk=obj.pk)
-        projects = project.integration.project_set.all()
-        for p in projects:
-            responsable = p.responsable_set.all().first()
-            g = Generated(change=obj,user=responsable.user)
+            
+        obj.save()
+        integrations = Integration.objects.filter(integrate__project_id=obj.project.pk)
+        
+        responsible_users=[]
+        for i in integrations:
+            responsible = User.objects.filter(responsable__project__integrate__integration_id=i.id)
+            responsible_users = list(set(responsible_users) | set(responsible)) 
+
+        logging.error("Responsables encontrados: %s" % responsible_users)
+        emails=[]
+        messages_to_responsible = ()
+        link_to_change = "%s%s%s" % (settings.BASE_URL, '/admin/app/change/', obj.pk)
+        message_subject = "Actualizacion de '%s'" % obj.project.name
+        message_from='noreply@rectorado.unl.edu.ar'
+        message_content = "Proyecto actualizado: %s\n\n" % obj.project.name
+        message_content += "Cambio: %s\n\n" % obj.name
+        message_content += "Descripcion: \n%s\n\n" % obj.description
+        message_content += "Creado: %s\n" % obj.created_at.strftime("%d/%m/%Y %H:%M")
+        message_content += "Modificado: %s\n" % obj.updated_at.strftime("%d/%m/%Y %H:%M")
+        message_content += "Confirmar cambio en: %s\n" % link_to_change
+
+        for u in responsible_users:
+            g = Generated(change=obj,user=u)
             g.save()
+
+            if u.email:
+                message_to = (message_subject,
+                               message_content,
+                               message_from,
+                               ["%s" % u.email])
+                messages_to_responsible += (message_to,)
+               
+        try:
+            sent_messages = send_mass_mail(messages_to_responsible, fail_silently=False)
+            messages.info(request, 'Se enviaron %s notificaciones correspondientes a los responsables de proyecto' % sent_messages)
+            logging.error("Sent messages:  %s" % sent_messages)
+        except Exception as e:
+            logging.error("ERROR to sent messajes. %s" % e)
 
             
 admin.site.register(Integration, IntegrationAdmin)
